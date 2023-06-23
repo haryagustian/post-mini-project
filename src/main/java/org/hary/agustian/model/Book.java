eae75b63349b5bdb87f1bd5a5df26a4e7dc80b84
Hary Agustian <tech.hary.agustian@gmail.com>
2023-06-23 20:01:29 +0700

    fix error

--- src/main/java/org/hary/agustian/model/Book.java
+++ src/main/java/org/hary/agustian/model/Book.java
@@ -11,7 +11,7 @@ public class Book implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;git
+    private Integer id;
     private String title;
     private Integer year;
 
